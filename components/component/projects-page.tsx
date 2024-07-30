d8588d82bc9b25d3327ecf47371cadac8a482718
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2024-07-30 14:20:59 +0530

    minorchange2

--- components/component/projects-page.tsx
+++ components/component/projects-page.tsx
@@ -23,7 +23,7 @@ To read more about using these font, please visit the Next.js documentation:
 - App Directory: https://nextjs.org/docs/app/building-your-application/optimizing/fonts
 - Pages Directory: https://nextjs.org/docs/pages/building-your-application/optimizing/fonts
 **/
-import Link from "next/link"
+import Link from "next/link";
 import { Card, CardHeader, CardContent, CardTitle, CardDescription } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 
