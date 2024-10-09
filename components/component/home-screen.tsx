9d920cda5286859c0c1d56527c74368a0e15a802
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2024-10-09 14:01:21 +0100

    ImageUpgrade

--- components/component/home-screen.tsx
+++ components/component/home-screen.tsx
@@ -158,7 +158,7 @@ export function HomeScreen({onNavigateToProject, onNavigateToAcademics, onNaviga
                     <Card>
                       <CardHeader>
                       <img
-                        src="/SupplierBizHacks.svg"
+                        src="/SupplierBizHacks.png"
                         srcSet="/SupplierBizHacks.svg 1x, /SupplierBizHacks@2x.svg 2x, /SupplierBizHacks@3x.svg 3x"
                         width="550"
                         height="310"
@@ -232,7 +232,7 @@ export function HomeScreen({onNavigateToProject, onNavigateToAcademics, onNaviga
                     <Card>
                       <CardHeader>
                       <img
-                        src="/Forze_FuelCell.svg"
+                        src="/Forze_FuelCell.png"
                         srcSet="/Forze_FuelCell.svg 1x, /Forze_FuelCell@2x.svg 2x, /Forze_FuelCell@3x.svg 3x"
                         width="550"
                         height="310"
